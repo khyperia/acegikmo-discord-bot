21821b595b90a44a1a5eaecd4ceb62dcc2b12870
khyperia <github@khyperia.com>
2021-01-15 21:18:55 +0100

    make snowflaketotime and timetosnowflake public

--- EchoCommand.cs
+++ EchoCommand.cs
@@ -15,12 +15,12 @@ namespace AcegikmoDiscordBot
                 var msg = message.Content.Substring("!echo ".Length).Trim('`');
                 await message.Channel.SendMessageAsync(msg);
             }
-            if (message.Author.Id == ASHL && message.Content.StartsWith("!snowflaketotime ") && ulong.TryParse(message.Content.Substring("!snowflaketotime ".Length), out var snowflake))
+            if (message.Content.StartsWith("!snowflaketotime ") && ulong.TryParse(message.Content.Substring("!snowflaketotime ".Length), out var snowflake))
             {
                 var time = SnowflakeUtils.FromSnowflake(snowflake);
                 await message.Channel.SendMessageAsync(time.DateTime.ToString("r"));
             }
-            if (message.Author.Id == ASHL && message.Content.StartsWith("!timetosnowflake ") && DateTime.TryParse(message.Content.Substring("!timetosnowflake ".Length), out var thingyTime))
+            if (message.Content.StartsWith("!timetosnowflake ") && DateTime.TryParse(message.Content.Substring("!timetosnowflake ".Length), out var thingyTime))
             {
                 var theSnowflake = SnowflakeUtils.ToSnowflake(thingyTime);
                 await message.Channel.SendMessageAsync(theSnowflake.ToString());
