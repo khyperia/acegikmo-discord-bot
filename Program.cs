081ae4eda11b33c69b69155bfc47b447acb0fd98
khyperia <github@khyperia.com>
2021-02-16 22:17:00 +0100

    nobody liked that

--- Program.cs
+++ Program.cs
@@ -16,12 +16,6 @@ namespace AcegikmoDiscordBot
     {
         [DataMember]
         internal string token;
-
-        [DataMember]
-        internal string twitchclientid;
-
-        [DataMember]
-        internal string twitchsecret;
     }
 #pragma warning restore CS0649
 #pragma warning restore CS8618 // Non-nullable field is uninitialized.
@@ -77,7 +71,6 @@ namespace AcegikmoDiscordBot
             _client.MessageReceived += new TimingThing(_log).MessageReceivedAsync;
             _client.MessageReceived += _log.MessageReceivedAsync;
             _client.MessageUpdated += _log.MessageUpdatedAsync;
-            new TwitchWatcher(_client);
 
             await _client.LoginAsync(TokenType.Bot, Config.token);
             await _client.StartAsync();
